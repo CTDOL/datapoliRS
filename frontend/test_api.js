e6a3f88faf0d5ddb742f30e309a84a3074b8589f
Jonas Henrique Spindler <198004216+jonasCTDOL@users.noreply.github.com>
2026-08-20 11:21:21 -0300

    chore: remove unused testing scripts and API test files

--- frontend/test_api.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-axios.get('http://localhost:8000/api/v1/gabinete/liderancas', {
-  headers: {
-    // I don't have the token. I can just test the DB query via python script.
-  }
-}).catch(e => console.log('Cannot fetch API directly without auth'));
